dc8d9d4aef6674ae060adfb4d112f9f8a0ae418d
almazheyderli <bd7g7vr18@code.edu.az>
2024-03-26 02:17:51 +0400

    employee and department

---
+++ ConsoleApp1/ConsoleApp1/Department.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Department
+    {
+        Employee[] Employees;
+        public void AddEmployee(Employee employee)
+        {
+            Array.Resize(ref Employees, Employees.Length + 1);
+        }
+        public void ShowEmployeeInfo()
+        {
+            for(int i = 0; i < Employees.Length; i++)
+            {
+                Employee employee = Employees[i];
+                Console.WriteLine($"{employee.Name},{employee.Surname},{employee.Age},{employee.DepartmentNo},{employee.Salary}");
+            }
+        }
+        public Employee[] GetEmployees()
+        {
+            return Employees;
+        } 
+        public Employee[] GetEmployeesBySalary(decimal min, decimal max)
+        {
+            Employee[] result = new Employee[] { };
+            foreach(Employee employee in Employees)
+            { 
+                if(employee.Salary > min && employee.Salary < max)
+                {
+                    Array.Resize(ref result,result.Length + 1);
+                }
+            }
+            return result;
+        }
+    }
+}
